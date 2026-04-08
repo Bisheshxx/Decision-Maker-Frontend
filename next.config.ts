a190a75f8945dcbe90dedb3412438d1e75e5b8c1
Bisheshxx <bishesh.tuladhar1@gmail.com>
2026-04-08 17:31:01 +1200

    fix: cookies issue

--- next.config.ts
+++ next.config.ts
@@ -3,6 +3,16 @@ import type { NextConfig } from "next";
 const nextConfig: NextConfig = {
   /* config options here */
   output: "standalone",
+  async rewrites() {
+    return [
+      {
+        // This matches any request starting with /api
+        source: "/api/:path*",
+        // This redirects it to your .NET backend behind the scenes
+        destination: `${process.env.NEXT_PUBLIC_API_URL}/api/:path*`,
+      },
+    ];
+  },
 };
 
 export default nextConfig;
