69d62f303f5d5930f39e513f75818359cac6fb8d
Bisheshxx <bishesh.tuladhar1@gmail.com>
2026-04-12 01:20:35 +1200

    fix: issue with middleware

--- src/proxy.ts
+++ src/proxy.ts
@@ -2,11 +2,28 @@ import { NextResponse } from "next/server";
 import { NextRequest } from "next/server";
 
 export function proxy(request: NextRequest) {
-  // const token = request.cookies.get("access_token");
-  // if (!token && !request.nextUrl.pathname.startsWith("/login")) {
-  //   return NextResponse.redirect(new URL("/login", request.url));
-  // }
-  // return NextResponse.next();
+  const hasRefreshToken = request.cookies.has("refresh_token");
+
+  const { pathname, searchParams } = request.nextUrl;
+  const isBlank = pathname === "/";
+  const isAuthPage =
+    pathname === "/login" || pathname === "/register" || isBlank;
+  const isAuthFailure = searchParams.get("code") === "401";
+
+  if (hasRefreshToken && isBlank) {
+    return NextResponse.redirect(new URL("/dashboard", request.url));
+  }
+
+  // Don't redirect back to dashboard if auth failed (code=401)
+  if (hasRefreshToken && isAuthPage && !isAuthFailure) {
+    return NextResponse.redirect(new URL("/dashboard", request.url));
+  }
+
+  if (!hasRefreshToken && pathname.startsWith("/dashboard")) {
+    return NextResponse.redirect(new URL("/login", request.url));
+  }
+
+  return NextResponse.next();
 }
 
 export const config = {
