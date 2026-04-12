2b8541112781df155261585e7e488e96abae729c
Bisheshxx <bishesh.tuladhar1@gmail.com>
2026-04-12 23:23:55 +1200

    fix: comented proxy ts

--- src/proxy.ts
+++ src/proxy.ts
@@ -2,26 +2,26 @@ import { NextResponse } from "next/server";
 import { NextRequest } from "next/server";
 
 export function proxy(request: NextRequest) {
-  const hasRefreshToken = request.cookies.has("refresh_token");
+  // const hasRefreshToken = request.cookies.has("refresh_token");
 
-  const { pathname, searchParams } = request.nextUrl;
-  const isBlank = pathname === "/";
-  const isAuthPage =
-    pathname === "/login" || pathname === "/register" || isBlank;
-  const isAuthFailure = searchParams.get("code") === "401";
+  // const { pathname, searchParams } = request.nextUrl;
+  // const isBlank = pathname === "/";
+  // const isAuthPage =
+  //   pathname === "/login" || pathname === "/register" || isBlank;
+  // const isAuthFailure = searchParams.get("code") === "401";
 
-  if (hasRefreshToken && isBlank) {
-    return NextResponse.redirect(new URL("/dashboard", request.url));
-  }
+  // if (hasRefreshToken && isBlank) {
+  //   return NextResponse.redirect(new URL("/dashboard", request.url));
+  // }
 
-  // Don't redirect back to dashboard if auth failed (code=401)
-  if (hasRefreshToken && isAuthPage && !isAuthFailure) {
-    return NextResponse.redirect(new URL("/dashboard", request.url));
-  }
+  // // Don't redirect back to dashboard if auth failed (code=401)
+  // if (hasRefreshToken && isAuthPage && !isAuthFailure) {
+  //   return NextResponse.redirect(new URL("/dashboard", request.url));
+  // }
 
-  if (!hasRefreshToken && pathname.startsWith("/dashboard")) {
-    return NextResponse.redirect(new URL("/login", request.url));
-  }
+  // if (!hasRefreshToken && pathname.startsWith("/dashboard")) {
+  //   return NextResponse.redirect(new URL("/login", request.url));
+  // }
 
   return NextResponse.next();
 }
